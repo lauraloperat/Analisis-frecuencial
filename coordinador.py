95a797b295b1f3fb58526e7cc84790b0a9e4de7d
lauraloperat <63822555+lauraloperat@users.noreply.github.com>
2020-04-18 13:42:37 -0500

    Update coordinador.py

--- coordinador.py
+++ coordinador.py
@@ -1,36 +1,36 @@
-"" "
+"""
 Juan Pablo Vasco y Laura Lopera 
-"" "
-de  Modelo  import  Biosenal
-desde  interfaz  import  InterfazGrafico
- sys de importación
-de  PyQt5 . QtWidgets  importa  QApplication
+"""
+from Modelo import Biosenal
+from interfaz import InterfazGrafico
+import sys
+from PyQt5.QtWidgets import QApplication
 
-Clase  Principal ( objeto ):
-    def  __init__ ( self ):        
-        auto . __app = QApplication ( sys . argv )
-        auto . __mi_vista = InterfazGrafico ()
-        auto . __mi_biosenal = Biosenal ()
-        auto . __mi_controlador = Coordinador ( self . __mi_vista , self . __mi_biosenal )
-        auto . __mi_vista . asignar_Controlador ( self . __mi_controlador )
-    def  main ( self ):
-        auto . __mi_vista . show ()
-        sys . salir ( self . __app . exec_ ())
+class Principal(object):
+    def __init__(self):        
+        self.__app=QApplication(sys.argv)
+        self.__mi_vista=InterfazGrafico()
+        self.__mi_biosenal=Biosenal()
+        self.__mi_controlador=Coordinador(self.__mi_vista,self.__mi_biosenal)
+        self.__mi_vista.asignar_Controlador(self.__mi_controlador)
+    def main(self):
+        self.__mi_vista.show()
+        sys.exit(self.__app.exec_())
     
- Coordinador de clase ( objeto ):
-    def  __init__ ( self , vista , biosenal ):
-        auto . __mi_vista = vista
-        auto . __mi_biosenal = biosenal
-    def  recibirDatosSenal ( self , data ):
-        auto . __mi_biosenal . asignarDatos ( datos )
-# print (data.shape)
-    def  devolverDatosSenal ( self , x_min , x_max ):
-        volver a  sí mismo . __mi_biosenal . devolver_segmento ( x_min , x_max )
-    def  escalarSenal ( self , x_min , x_max , escala ):
-        volver a  sí mismo . __mi_biosenal . escalar_senal ( x_min , x_max , escala )
-    def  graficar_canal ( auto , canal ):
-        volver a  sí mismo . graficar_canal ( canal )
-     filtro de def ( self , senal , tipo_umbral , umbral , ponderado ):
-        volver a  sí mismo . __mi_biosenal . filtrar ( senal , tipo_umbral , umbral , ponderado )
-p = Principal ()
-p . main ()
+class Coordinador(object):
+    def __init__(self,vista,biosenal):
+        self.__mi_vista=vista
+        self.__mi_biosenal=biosenal
+    def recibirDatosSenal(self,data):
+        self.__mi_biosenal.asignarDatos(data)
+#        print(data.shape)
+    def devolverDatosSenal(self,x_min,x_max):
+        return self.__mi_biosenal.devolver_segmento(x_min,x_max)
+    def escalarSenal(self,x_min,x_max,escala):
+        return self.__mi_biosenal.escalar_senal(x_min,x_max,escala)
+    def graficar_canal(self,canal):
+        return self.graficar_canal(canal)
+    def filtrar(self, senal, tipo_umbral, umbral, ponderado):
+        return self.__mi_biosenal.filtrar(senal, tipo_umbral, umbral, ponderado)
+p=Principal()
+p.main()
