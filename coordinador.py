e8c424c5e1681fa5c61d961a16e94cfd566f8877
lauraloperat <63822555+lauraloperat@users.noreply.github.com>
2020-04-22 21:48:40 -0500

    Codigo comentado

--- coordinador.py
+++ coordinador.py
@@ -7,7 +7,7 @@ import sys
 from PyQt5.QtWidgets import QApplication
 
 class Principal(object):
-    def __init__(self):
+    def __init__(self):        
         self.__app=QApplication(sys.argv)
         self.__mi_vista=InterfazGrafico()
         self.__mi_biosenal=Biosenal()
@@ -16,23 +16,21 @@ class Principal(object):
     def main(self):
         self.__mi_vista.show()
         sys.exit(self.__app.exec_())
-
+    
 class Coordinador(object):
     def __init__(self,vista,biosenal):
         self.__mi_vista=vista
         self.__mi_biosenal=biosenal
     def recibirDatosSenal(self,data):
         self.__mi_biosenal.asignarDatos(data)
-#        print(data.shape)
     def devolverDatosSenal(self,x_min,x_max):
         return self.__mi_biosenal.devolver_segmento(x_min,x_max)
     def escalarSenal(self,x_min,x_max,escala):
         return self.__mi_biosenal.escalar_senal(x_min,x_max,escala)
     def calcularWavelet(self,x,fmin,fmax,fs):
         return self.__mi_biosenal.calcularWavelet(x,fmin,fmax,fs)
-    def graficar_canal(self,canal):
-        return self.graficar_canal(canal)
-    def filtrar(self, senal, tipo_umbral, umbral, ponderado):
-        return self.__mi_biosenal.filtrar(senal, tipo_umbral, umbral, ponderado)
+    
+    
 p=Principal()
 p.main()
+
