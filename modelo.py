90d34df9ffaf4a1a7f6dfb5d3cc9555f95a25125
lauraloperat <63822555+lauraloperat@users.noreply.github.com>
2020-04-22 21:57:08 -0500

    Codigo comentado

--- modelo.py
+++ modelo.py
@@ -1,10 +1,7 @@
-# -*- coding: utf-8 -*-
 """
 Juan Pablo Vasco y Laura Lopera
 """
 import numpy as np
-import scipy.io as sio
-import matplotlib.pyplot as plt
 import pywt #1.1.1
 
 
@@ -20,7 +17,7 @@ class Biosenal(object):
         self.__data=data
         self.__canales=data.shape[0]
         self.__puntos=data.shape[1]
-    #necesitamos hacer operacioes basicas sobre las senal, ampliarla, disminuirla, trasladarla temporalmente etc
+   
     def devolver_segmento(self,x_min,x_max):
         '''
         Parameters
@@ -35,36 +32,50 @@ class Biosenal(object):
         '''
         if x_min>=x_max:
             return None #si el valor minimo del rango es mayor o igual al maximo, no retorna nada
-        #cojo los valores que necesito en la biosenal
+        #se toman los valores que se necesitan en la biosenal
 
-        return self.__data[:,x_min:x_max]
+        return self.__data[:,x_min:x_max] 
+    
     def calcularWavelet(self,senal,fmin,fmax,fs):
+        """        
+        Parameters
+        ----------
+        senal : datos a los que se le aplicaran el Wavelet continuo.
+        fmin : frecuencia minima de interes.
+        fmax : frecuencia maxima de interes.
+        fs : frecuencia de muestreo de la senal.
 
+        Returns
+        -------
+        time : rango de tiempo del espectro.
+        freqs : rengo de frecuencias del espectro.
+        power : rango de potencias del espectro.
+
+        """
+                        
         #analisis usando wavelet continuo
 
         sampling_period =  1/fs
         Frequency_Band = [fmin, fmax] # Banda de frecuencia a analizar
-
-        # Métodos de obtener las escalas para el Complex Morlet Wavelet
-        # Método 1:
-        # Determinar las frecuencias respectivas para una escalas definidas
+        
+        
+        # se determinan las frecuencias respectivas para una escala definida
         scales = np.arange(1, 250)
         frequencies = pywt.scale2frequency('cmor', scales)/sampling_period
-        # Extraer las escalas correspondientes a la banda de frecuencia a analizar
-        scales = scales[(frequencies >= Frequency_Band[0]) & (frequencies <= Frequency_Band[1])]
-
+        # se extraen las escalas correspondientes a la banda de frecuencia a analizar
+        scales = scales[(frequencies >= Frequency_Band[0]) & (frequencies <= Frequency_Band[1])] 
+        
         N = senal.shape[0]
-
-        # Obtener el tiempo correspondiente a una epoca de la señal (en segundos)
+    
+        # se obtiene el tiempo correspondiente a una epoca de la senal
         time_epoch = sampling_period*N
-
-        # Analizar una epoca de un montaje (con las escalas del método 1)
-        # Obtener el vector de tiempo adecuado para una epoca de un montaje de la señal
+        
+        # se analiza una epoca 
+        # se obtiene el vector de tiempo
         time = np.arange(0, time_epoch, sampling_period)
-        # Para la primera epoca del segundo montaje calcular la transformada continua de Wavelet, usando Complex Morlet Wavelet
-
+        
         [coef, freqs] = pywt.cwt(senal, scales, 'cmor', sampling_period)
-        # Calcular la potencia
+        # se calcula la potencia 
         power = (np.abs(coef)) ** 2
-
+        
         return time, freqs, power
